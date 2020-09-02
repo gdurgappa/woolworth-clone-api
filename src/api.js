1b461006c630b0ba00eae1ba0f89bb70bf1690c6
gdurgappa <g.durgappa5000@gmail.com>
2020-09-02 14:50:07 +1000

    adding cors

--- src/api.js
+++ src/api.js
@@ -2,9 +2,13 @@ const express = require("express");
 const serverless = require("serverless-http");
 const getDataFromWoolyApi = require("./controller");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 const router = express.Router();
+
+app.use(cors());
+app.options("*", cors());
 
 router.get("/", (req, res) => {
   res.json({
