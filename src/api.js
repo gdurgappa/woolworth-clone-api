5ad8163152b9161d4173111b919a0e2455cc6bbe
gdurgappa <g.durgappa5000@gmail.com>
2020-09-02 14:45:59 +1000

    build fix error 2

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const getDataFromWoolyApi = require("./controller");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -12,6 +13,10 @@ router.get("/", (req, res) => {
 });
 router.post("/getDataFromWoolyApi", getDataFromWoolyApi);
 app.use(`/.netlify/functions/api`, router);
+
+app.listen(4000, () => {
+  console.log(`App listening on the port 4000`);
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
